ff935ce6de11bc4fd1e385f8d0c8e2aa056272d0
ayumitanaka <tanakayumi13@gmail.com>
2021-02-11 17:31:11 -0800

    modified index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ $(function () {
 //   $(".menu").removeClass("open");
 // }
 
-
 $(".shopping-cart").click(function () {
   $(this).toggleClass("on");
   $(".cart").slideToggle();
@@ -22,12 +21,12 @@ $(".shopping-cart").click(function () {
 // });
 
 // page slide
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,    
-  }
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
 });
 
 //// api
@@ -38,62 +37,63 @@ getItems();
 
 // getItems
 async function getItems() {
-  const res = await axios.get('https://demo-api-project01.herokuapp.com/api/products');
-  responseData = res.data
+  const res = await axios.get(
+    "https://demo-api-project01.herokuapp.com/api/products"
+  );
+  responseData = res.data;
   showItems(responseData);
   showTags(responseData);
 }
 
 // showItems
 function showItems(datas) {
-
-  const iterate = datas.length/6; 
+  const iterate = datas.length / 6;
   let counter = 0;
-  const cardsWrapper = document.querySelector('.swiper-wrapper');
-  cardsWrapper.innerHTML = '';
+  const cardsWrapper = document.querySelector(".swiper-wrapper");
+  cardsWrapper.innerHTML = "";
 
-    for (let i = 0; i < iterate; i++) {
-      //create the swipper-slide div
-      const cards = document.createElement('div');
-      cards.classList.add('swiper-slide');
-      cards.classList.add('cards');
+  for (let i = 0; i < iterate; i++) {
+    //create the swipper-slide div
+    const cards = document.createElement("div");
+    cards.classList.add("swiper-slide");
+    cards.classList.add("cards");
 
-      for(let j = 0; j < 6; j++){
-        //create the card div to display each card
-        const {name, price, description, image_url} = datas[counter];
-        const card = document.createElement('div');
-        card.classList.add('card');
-        const cardText = document.createElement('div');
-        cardText.classList.add('card_text');
-    
-        const itemTitle = document.createElement('h6');
-        itemTitle.classList.add('item_title')
-        itemTitle.innerHTML = `${name}`
-        cardText.appendChild(itemTitle)
-    
-        const addItemWrapper = document.createElement('div')
-        cardText.appendChild(addItemWrapper)
-        
-        const shoppingCart = document.createElement('i')
-        shoppingCart.classList.add('fas')
-        shoppingCart.classList.add('fa-shopping-cart')
-        shoppingCart.setAttribute('id', 'add_item')
-        addItemWrapper.appendChild(shoppingCart);
-    
-        // add clicked item to cart
-        shoppingCart.addEventListener("click", addToCart);
-    
-        const priceSign = document.createElement('h6');
-        priceSign.classList.add('price_sign')
-        priceSign.innerHTML = `
+    for (let j = 0; j < 6; j++) {
+      //create the card div to display each card
+      const { name, price, description, image_url } = datas[counter];
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const cardText = document.createElement("div");
+      cardText.classList.add("card_text");
+
+      const itemTitle = document.createElement("h6");
+      itemTitle.classList.add("item_title");
+      itemTitle.innerHTML = `${name}`;
+      cardText.appendChild(itemTitle);
+
+      const addItemWrapper = document.createElement("div");
+      cardText.appendChild(addItemWrapper);
+
+      const shoppingCart = document.createElement("i");
+      shoppingCart.classList.add("fas");
+      shoppingCart.classList.add("fa-shopping-cart");
+      shoppingCart.setAttribute("id", "add_item");
+      addItemWrapper.appendChild(shoppingCart);
+
+      // add clicked item to cart
+      shoppingCart.addEventListener("click", addToCart);
+
+      const priceSign = document.createElement("h6");
+      priceSign.classList.add("price_sign");
+      priceSign.innerHTML = `
             $
             <span id="price">
               ${price}
             </span>
-        `
-        addItemWrapper.appendChild(priceSign);
-    
-        card.innerHTML = `
+        `;
+      addItemWrapper.appendChild(priceSign);
+
+      card.innerHTML = `
               <div class="gallery_img">
                 <img id="item_image" src="${image_url}" alt="${name}">
                   <div class="caption">
@@ -102,19 +102,18 @@ function showItems(datas) {
                     </p>
                   </div>
               </div>
-        `
-        
-        card.appendChild(cardText)
-        cards.appendChild(card);
-        arr.push(card);
-              
-        datas[counter];
-        counter++;
-        
-      }
-      cardsWrapper.appendChild(cards);
+        `;
+
+      card.appendChild(cardText);
+      cards.appendChild(card);
+      arr.push(card);
+
+      datas[counter];
+      counter++;
     }
-    swiper.update()
+    cardsWrapper.appendChild(cards);
+  }
+  swiper.update();
 }
 
 // showTags
@@ -124,21 +123,21 @@ function showTags(datas) {
     tagsArr.push(data.category);
     tags = tagsArr.filter(function (x, i, self) {
       return self.indexOf(x) === i;
-    })
-  })
+    });
+  });
   tags.forEach((tag) => {
-    const categories = document.querySelector('.categories');
-    const btn = document.createElement('button');
-    btn.classList.add('category');
+    const categories = document.querySelector(".categories");
+    const btn = document.createElement("button");
+    btn.classList.add("category");
     btn.innerText = `# ${tag}`;
     categories.append(btn);
-});
+  });
 }
 
 // search item
-const search = document.querySelector('.search');
+const search = document.querySelector(".search");
 
-search.addEventListener('input', function(e) {
+search.addEventListener("input", function (e) {
   findItem(e.target.value);
 });
 
@@ -148,12 +147,14 @@ function findItem(searchItem) {
       return item;
     } else if (item.category.toLowerCase().includes(searchItem.toLowerCase())) {
       return item;
-    } else if (item.description.toLowerCase().includes(searchItem.toLowerCase())) {
+    } else if (
+      item.description.toLowerCase().includes(searchItem.toLowerCase())
+    ) {
       return item;
     } else {
       return;
     }
-  })
+  });
   showItems(filteredItems);
   console.log(filteredItems);
 }
@@ -175,54 +176,54 @@ function findItem(searchItem) {
 //   })
 // }
 
-
 // --------------------------- cart function ----------------------------
 
 const cart_items = document.querySelectorAll("#cart_items");
 
-// remove a item 
-const removeItemButtons = document.querySelectorAll("#cart_trash")
-.forEach(()=> {
-  removeButton.addEventListener("click", removeCartItem);
-})
+// remove a item
+const removeItemButtons = document
+  .querySelectorAll("#cart_trash")
+  .forEach(() => {
+    removeButton.addEventListener("click", removeCartItem);
+  });
 
 // for (let removeButton of removeItemButtons) {
 //   removeButton.addEventListener("click", removeCartItem);
 // }
 
-// change quantity 
+// change quantity
 const quantityInputs = document.querySelectorAll("#cart_quantity");
 for (let input of quantityInputs) {
   input.addEventListener("change", quantityChanged);
 }
 
 // click checkout
-const checkout = document.querySelector('#cart_checkout');
-checkout.addEventListener('click', function() {
+const checkout = document.querySelector("#cart_checkout");
+checkout.addEventListener("click", function () {
   alert("Thank you for shopping with us!");
   while (cart_items.hasChildNodes()) {
     cart_items.removeChild(cart_items.firstChild);
   }
   updateCartQtyTotal();
-})
+});
 
 // remove item from the cart
-function removeCartItem(e) {
-  document.querySelectorAll('cart_item_title').forEach((ele)=> {
+function removeCartItem(parentEle, title) {
+  document.querySelectorAll("#cart_item_title").forEach((ele) => {
+    const cart_item_titles = document.querySelectorAll("#cart_item_title");
 
-    const cart_item_titles = document.querySelectorAll('#cart_item_title') 
-    console.log(e.target);
+    const want_to_delete_item = parentEle;
+    const delete_item_title = title;
 
-    const delete_item_title = e.target.attributes.alt.textContent;
-    console.log(delete_item_title);
-      for (cart_item_title of cart_item_titles) {
-        console.log(cart_item_title.innerText.trim());
-        if (cart_item_title.innerText === delete_item_title) {
-          e.target.remove(ele);
-        }
+    for (cart_item_title of cart_item_titles) {
+      console.log(cart_item_title.innerText.trim());
+      if (cart_item_title.innerText === delete_item_title) {
+        want_to_delete_item.removeChild(ele);
+        console.log(want_to_delete_item);
+        // e.target.remove(ele);
       }
-    
-  })
+    }
+  });
 
   updateCartQtyTotal();
 }
@@ -236,7 +237,7 @@ function quantityChanged(e) {
   updateCartQtyTotal();
 }
 
-// get clicked item 
+// get clicked item
 function addToCart(e) {
   const addButtonClicked = e.target;
   const shopItem = addButtonClicked.parentElement.parentElement.parentElement;
@@ -260,9 +261,9 @@ function addItemToCart(title, price, imageSrc) {
 
   // to create cart_item for laptop and mobile
   for (cart_item of cart_items) {
-    const cart = document.createElement('div');
-    cart.classList.add('cart');
-    cart.classList.add('cart_item');
+    const cart = document.createElement("div");
+    cart.classList.add("cart");
+    cart.classList.add("cart_item");
     cart.innerHTML = `
       <ul id="cart_item"> 
         <li id="cart_item_img">
@@ -282,7 +283,7 @@ function addItemToCart(title, price, imageSrc) {
         </li>
       </ul>
     `;
-    cart_item.appendChild(cart);    
+    cart_item.appendChild(cart);
   }
 
   const cart_trashes = document.querySelectorAll("#cart_trash");
@@ -290,14 +291,15 @@ function addItemToCart(title, price, imageSrc) {
 
   // to add remove func for laptop and mobile
   for (cart_trash of cart_trashes) {
-    cart_trash.addEventListener("click", removeCartItem);
+    const parentRow = cart_trash.childNodes[2].parentNode.parentNode.parentNode;
+
+    cart_trash.addEventListener("click", removeCartItem(parentRow, title));
   }
   // to add quantity func for laptop and mobile
   for (cart_quantity of cart_quantities) {
     cart_quantity.addEventListener("change", quantityChanged);
   }
 }
-
 
 function updateCartQtyTotal() {
   const carts = document.querySelectorAll("#cart_item");
@@ -307,9 +309,9 @@ function updateCartQtyTotal() {
   // console.log(cart_totals);
   // console.log(cart_total_prices);
 
-  let total = 0.00;
+  let total = 0.0;
   let quantity = 0;
-  let cart_total = 0.00;
+  let cart_total = 0.0;
 
   for (cart of carts) {
     const priceEl = cart.querySelector("#cart_price");
@@ -339,11 +341,10 @@ function updateCartQtyTotal() {
     // console.log(cart_total);
     // quantity += quantity;
     // document.querySelectorAll("#cart_total_price").innerText = `Total $ ${total}`;
-    
+
     // console.log(totalPriceEl);
     // totalPriceEl.innerText = `Total $ ${total}`;
   }
-
 
   // to show same quantities on laptop and mobile
   // const quantityEls = document.querySelectorAll("#cart_quantity");
@@ -360,10 +361,9 @@ function updateCartQtyTotal() {
   // console.log(quantityEls[0].value);
   // console.log(quantityEls[1].value);
 
-    // for (q of quantityEls) {
-    //   console.log(q.value);
-    // }
-
+  // for (q of quantityEls) {
+  //   console.log(q.value);
+  // }
 
   // total += price * quantity;
   // console.log(total);
@@ -373,17 +373,15 @@ function updateCartQtyTotal() {
 
   total = Math.round(total * 100) / 100;
   // document.querySelectorAll("#cart_total_price").innerText = `Total $ ${total}`;
-  
+
   for (cart_total_price of cart_total_prices) {
-
-
     // console.log(total);
     // console.log(cart_total_price.innerText);
-    
+
     cart_total_price.innerText = `Total $ ${total}`;
     // console.log(cart_total_price);
     // total = total + cart_total_price;
-    // console.log(total);    
+    // console.log(total);
   }
 
   // cart_total_price = total;
@@ -391,5 +389,4 @@ function updateCartQtyTotal() {
   // console.log(cart_total_price);
   // total = total + cart_total_price;
   // console.log(document.querySelectorAll("#cart_total_price"));
-
 }
